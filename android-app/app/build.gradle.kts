7f9e6fca52827c9941f32fb94905a59ddc0a080a
yaseen2 <yaseenwazir953@gmail.com>
2026-06-07 12:57:07 +0500

    Fix debug signing error: Remove custom debugConfig and fallback to Gradle's default debug keystore signing

--- android-app/app/build.gradle.kts
+++ android-app/app/build.gradle.kts
@@ -28,12 +28,7 @@ android {
       keyAlias = "upload"
       keyPassword = System.getenv("KEY_PASSWORD")
     }
-    create("debugConfig") {
-      storeFile = file("${rootDir}/debug.keystore")
-      storePassword = "android"
-      keyAlias = "androiddebugkey"
-      keyPassword = "android"
-    }
+
   }
 
   buildTypes {
@@ -44,7 +39,6 @@ android {
       signingConfig = signingConfigs.getByName("release")
     }
     debug {
-      signingConfig = signingConfigs.getByName("debugConfig")
     }
   }
   compileOptions {
