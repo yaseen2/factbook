6c0ca191b010121904721d06e4d7d5ef90e21c48
yaseen2 <yaseenwazir953@gmail.com>
2026-06-07 17:38:36 +0500

    Fix compile error: Import BorderStroke in ShareActivity.kt

--- android-app/app/src/main/java/com/scholar/factbook/ShareActivity.kt
+++ android-app/app/src/main/java/com/scholar/factbook/ShareActivity.kt
@@ -8,6 +8,7 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
